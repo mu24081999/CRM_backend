3ca9bd7b2cb97358fd7ad48729ee01a0cd51ab8b
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-03-06 02:18:21 +0500

    updated

--- controllers/email.js
+++ controllers/email.js
@@ -17,103 +17,126 @@ exports.sendEmail = catchAssyncFunc(async function (req, res, next) {
       pass: config.EMAIL_FROM_ACC_PASS,
     },
   });
-  let is_email_sent;
-  let is_email_saved;
-  let is_email_attachments_added;
-
+  const saveAttachments = async (data) => {
+    const is_added = await db("email_attachments").insert({
+      file_link: data.file_link,
+      size: data.size,
+      file_type: data.file_type,
+      email_id: data.email_id,
+    });
+    if (!is_added) {
+      reject(new Error("Error Uploading File: " + err));
+      return helper.sendError(
+        req,
+        res,
+        "Something went wrong while uploading.",
+        500
+      );
+    }
+    return is_added;
+  };
   const uploadFiles = async (files, is_email_added) => {
+    let is_email_attachments_added;
+    const email_attachments = [];
     if (Array.isArray(files)) {
-      // Multiple files selected
-      files.forEach(async (fileItem, index) => {
-        // Read file content
-        fs.readFile(fileItem.tempFilePath, async (err, data) => {
-          if (err) {
-            console.error("Error reading file:", err);
-            return res.status(500).send("Internal Server Error");
-          }
-          // Handle each fileItem here
-          const params = {
-            Bucket: config.S3_BUCKET,
-            Key: fileItem.name,
-            Body: data,
-            file_type: fileItem.mimetype,
-            ContentType: fileItem.mimetype,
-          };
-          console.log(params);
-
-          const is_added = await new Promise((resolve, reject) => {
-            s3.upload(params, {}, async (err, data) => {
+      is_email_attachments_added = await Promise.all(
+        files.map(async (file) => {
+          return new Promise((resolve, reject) => {
+            fs.readFile(file.tempFilePath, async (err, data) => {
               if (err) {
-                console.error(err);
-                reject(new Error("Error Uploading File: " + err));
-              } else {
-                const is_added = await db("email_attachments").insert({
-                  file_link: data.Location,
-                  size: fileItem.size,
-                  file_type: fileItem.mimetype,
-                  email_id: is_email_added[0],
-                });
-                if (!is_added) {
+                console.error("Error reading file:", err);
+                reject(new Error("Error reading file: " + err));
+                return helper.sendError(
+                  req,
+                  res,
+                  "Error uploading files.",
+                  500
+                );
+              }
+              const params = {
+                Bucket: config.S3_BUCKET,
+                Key: "emails/attachments/" + from + "/" + file.name,
+                Body: data,
+                ContentType: file.mimetype,
+              };
+              s3.upload(params, {}, async (err, data) => {
+                if (err) {
+                  console.error(err);
                   reject(new Error("Error Uploading File: " + err));
                   return helper.sendError(
                     req,
                     res,
-                    "Something went wrong while uploading.",
+                    "Error uploading files.",
                     500
                   );
                 }
+
+                email_attachments.push({
+                  filename: data.key,
+                  path: data.Location,
+                  size: file.size,
+                  file_type: file.mimetype,
+                });
                 resolve(true);
-              }
+              });
             });
           });
-        });
-      });
+        })
+      );
+      return {
+        status: is_email_attachments_added,
+        attachments: email_attachments,
+      };
     } else {
-      const is_added = await new Promise((resolve, reject) => {
-        const { name, tempFilePath, mimetype, size } = files;
+      const { tempFilePath, size, mimetype, name } = files;
+      is_email_attachments_added = await new Promise((resolve, reject) => {
         fs.readFile(tempFilePath, async (err, data) => {
           if (err) {
             console.error("Error reading file:", err);
-            return res.status(500).send("Internal Server Error");
+            return helper.sendError(req, res, "Error uploading files.", 500);
           }
           const params = {
             Bucket: config.S3_BUCKET,
-            Key: name,
+            Key: "emails/attachments/" + from + "/" + name,
             Body: data,
             ContentType: mimetype,
           };
           s3.upload(params, {}, async (err, data) => {
+            console.log("🚀 ~ s3.upload ~ data:", data);
             if (err) {
               console.error(err);
-              reject(new Error("Error Uploading Fie: " + err));
-            } else {
-              const is_added = await db("email_attachments").insert({
-                file_link: data.Location,
-                size: size,
-                file_type: mimetype,
-                email_id: is_email_added[0],
-              });
-              if (!is_added) {
-                reject(new Error("Error Uploading File: " + err));
-                return helper.sendError(
-                  req,
-                  res,
-                  "Something went wrong while uploading.",
-                  500
-                );
-              }
-              resolve(true);
+              reject(new Error("Error Uploading File: " + err));
             }
+            email_attachments.push({
+              filename: data.key,
+              path: data.Location,
+              size: size,
+              file_type: mimetype,
+            });
+            resolve(true);
           });
         });
       });
+      if (!is_email_attachments_added) {
+        return helper.sendError(req, res, "Error uploading attachments.", 500);
+      }
+      console.log("Uploading attachments", {
+        status: is_email_attachments_added,
+        attachments: email_attachments,
+      });
+      return {
+        status: is_email_attachments_added,
+        attachments: email_attachments,
+      };
     }
   };
   const sendEmail = async (mailOptions) => {
+    console.log("🚀 ~ sendEmail ~ mailOptions:", mailOptions);
     const emailResponse = await transporter.sendMail(mailOptions); // Upload file to S3
     return emailResponse;
   };
   const saveEmail = async (emailData) => {
+    console.log("🚀 ~ saveEmail ~ emailData:", emailData);
     const is_email_added = await db("emails").insert(emailData);
     if (!is_email_added) {
       return helper.sendError(req, res, "Error sending email.", 500);
@@ -122,79 +145,206 @@ exports.sendEmail = catchAssyncFunc(async function (req, res, next) {
   };
 
   if (Array.isArray(to)) {
-    to.forEach(async (toEmail) => {
-      const mailOptions = {
-        from: from,
-        to: toEmail,
-        subject: subject,
-        text: body,
-        // html: htmlMessage,
-      };
-      const emailResponse = await sendEmail(mailOptions);
-      let emailData;
-      if (type === "email") {
-        emailData = {
-          subject: subject,
-          body: body,
-          sender: from,
-          reciever: emailResponse.envelope.to,
-        };
-      } else if (type === "reply") {
-        emailData = {
-          subject: subject,
-          body: body,
-          sender: from,
-          reciever: emailResponse.envelope.to,
-          parent_id: parent_id,
-        };
-      }
-      const email_response = await saveEmail(emailData);
-      if (req.files) {
-        const { files } = req.files;
-        await uploadFiles(files, email_response);
-      }
-    });
-  } else {
-    const mailOptions = {
-      from: from,
-      to: to,
-      subject: subject,
-      text: body,
-      // html: htmlMessage,
-    };
-    const emailResponse = await sendEmail(mailOptions);
-    let emailData;
-    if (type === "email") {
-      emailData = {
-        subject: subject,
-        body: body,
-        sender: from,
-        reciever: emailResponse.envelope.to,
-      };
-    } else if (type === "reply") {
-      emailData = {
-        subject: subject,
-        body: body,
-        sender: from,
-        reciever: emailResponse.envelope.to,
-        parent_id: parent_id,
-      };
-    }
+    is_loop_completed = await Promise.all(
+      to.map(async (toEmail) => {
+        return new Promise(async (resolve, reject) => {
+          let email_response;
+          let emailResponse;
+          let is_email_attachments_added;
 
-    const email_response = await saveEmail(emailData);
+          if (req.files) {
+            const { files } = req.files;
+            is_email_attachments_added = await uploadFiles(files);
+          }
+          let mailOptions = {};
+          if (req.files && is_email_attachments_added) {
+            console.log(
+              "Uploading email attachments",
+              is_email_attachments_added?.attachments
+            );
+            mailOptions = {
+              from: from,
+              to: toEmail,
+              subject: subject,
+              // text: body,
+              attachments:
+                is_email_attachments_added &&
+                is_email_attachments_added?.attachments,
+              // [
+              //   {
+              //     // use URL as an attachment
+              //     filename: "license.txt",
+              //     path: "https://raw.github.com/nodemailer/nodemailer/master/LICENSE",
+              //   },
+              //   {
+              //     // use URL as an attachment
+              //     filename: "license.txt",
+              //     path: "https://raw.github.com/nodemailer/nodemailer/master/LICENSE",
+              //   },
+              // ],
+
+              html: body,
+            };
+          } else {
+            mailOptions = {
+              from: from,
+              to: toEmail,
+              subject: subject,
+              // text: body,
+              html: body,
+            };
+          }
+
+          emailResponse = await sendEmail(mailOptions);
+          let emailData = {}; // Initialize emailData to an empty object
+          if (emailResponse) {
+            emailData = {
+              subject: subject,
+              body: body,
+              sender: from,
+              ...(type === "reply" && emailResponse
+                ? {
+                    reciever: emailResponse.envelope.to,
+                    parent_id: parent_id,
+                  }
+                : {
+                    reciever: emailResponse.envelope.to,
+                  }),
+            };
+          }
+          email_response = await saveEmail(emailData);
+          let is_attachments_saved;
+          if (
+            req.files &&
+            is_email_attachments_added &&
+            is_email_attachments_added?.attachments?.length > 0 &&
+            email_response
+          ) {
+            is_attachments_saved = await Promise.all(
+              is_email_attachments_added?.attachments?.map((file, inex) => {
+                console.log(
+                  "🚀 ~ is_email_attachments_added?.attachments?.map ~ file:",
+                  file
+                );
+                return new Promise(async (resolve, reject) => {
+                  const is_added = await saveAttachments({
+                    email_id: email_response[0],
+                    file_link: file.path || file.href,
+                    size: file.size,
+                    file_type: file.file_type,
+                  });
+                  if (is_added) {
+                    resolve(true);
+                  }
+                });
+              })
+            );
+          }
+          if (
+            emailResponse &&
+            email_response &&
+            (req.files
+              ? is_email_attachments_added.status && is_attachments_saved
+              : true)
+          ) {
+            console.log("type: " + type);
+            resolve(true);
+          }
+        });
+      })
+    );
+    if (is_loop_completed.every((completed) => completed)) {
+      return helper.sendSuccess(req, res, {}, "Emails sent successfully.");
+    }
+  } else {
+    let email_response;
+    let emailResponse;
+    let is_email_attachments_added;
 
     if (req.files) {
       const { files } = req.files;
-      await uploadFiles(files, email_response);
+      is_email_attachments_added = await uploadFiles(files);
+    }
+    let mailOptions = {};
+    if (req.files && is_email_attachments_added) {
+      console.log(
+        "Uploading email attachments",
+        is_email_attachments_added?.attachments
+      );
+      mailOptions = {
+        from: from,
+        to: to,
+        subject: subject,
+        // text: body,
+        html: body,
+        attachments:
+          is_email_attachments_added && is_email_attachments_added?.attachments,
+      };
+    } else {
+      mailOptions = {
+        from: from,
+        to: to,
+        subject: subject,
+        // text: body,
+        html: body,
+      };
+    }
+
+    emailResponse = await sendEmail(mailOptions);
+    let emailData = {}; // Initialize emailData to an empty object
+    if (emailResponse) {
+      emailData = {
+        subject: subject,
+        body: body,
+        sender: from,
+        ...(type === "reply" && emailResponse
+          ? {
+              reciever: emailResponse.envelope.to,
+              parent_id: parent_id,
+            }
+          : {
+              reciever: emailResponse.envelope.to,
+            }),
+      };
+    }
+    email_response = await saveEmail(emailData);
+    let is_attachments_saved;
+    if (
+      req.files &&
+      is_email_attachments_added &&
+      is_email_attachments_added?.attachments?.length > 0 &&
+      email_response
+    ) {
+      is_attachments_saved = await Promise.all(
+        is_email_attachments_added?.attachments?.map((file, inex) => {
+          console.log(
+            "🚀 ~ is_email_attachments_added?.attachments?.map ~ file:",
+            file
+          );
+          return new Promise(async (resolve, reject) => {
+            const is_added = await saveAttachments({
+              email_id: email_response[0],
+              file_link: file.path || file.href,
+              size: file.size,
+              file_type: file.file_type,
+            });
+            if (is_added) {
+              resolve(true);
+            }
+          });
+        })
+      );
+    }
+    if (
+      emailResponse &&
+      email_response &&
+      (req.files
+        ? is_email_attachments_added.status && is_attachments_saved
+        : true)
+    ) {
+      return helper.sendSuccess(req, res, {}, "Emails sent successfully.");
     }
   }
-  return helper.sendSuccess(
-    req,
-    res,
-    // { emailData: emailResponse },
-    {},
-    "Email sent successfully."
-  );
 });
 
 exports.updateEmail = catchAssyncFunc(async function (req, res, next) {
@@ -205,7 +355,7 @@ exports.updateEmail = catchAssyncFunc(async function (req, res, next) {
     is_read: isRead,
     status: status,
   });
-  return helper.sendSuccess(req, res, {}, "Email updated successfully.");
+  return helper.sendSuccess(req, res, {}, "Success.");
 });
 exports.getEmails = catchAssyncFunc(async function (req, res, next) {
   const user = await db("users").where("id", req.user.id).first();
