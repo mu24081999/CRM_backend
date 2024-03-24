fa7479a43296bd5308937fd423b519803e1d968d
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-03-25 01:58:47 +0500

    updated

--- knexfile.js
+++ knexfile.js
@@ -1,29 +1,27 @@
 global.base_path = __dirname;
 global.config = require(base_path + "/config");
 module.exports = {
-  development: {
-    client: "mysql2",
-    connection: {
-      host: config.DB_HOST,
-      user: config.DB_USER,
-      password: config.DB_PASSWORD,
-      port: config.DB_PORT,
-      database: config.DB_NAME,
-      timezone: "+05:00", // Set the timezone for Pakistan
+  client: "mysql2",
+  connection: {
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    port: config.DB_PORT,
+    database: config.DB_NAME,
+    timezone: "+05:00", // Set the timezone for Pakistan
+  },
+  pool: {
+    afterCreate: function (connection, callback) {
+      connection.query("SET time_zone = '+05:00';", function (err) {
+        callback(err, connection);
+      });
     },
-    pool: {
-      afterCreate: function (connection, callback) {
-        connection.query("SET time_zone = '+05:00';", function (err) {
-          callback(err, connection);
-        });
-      },
-    },
-    // connection: config.POSTGRESS_URL,
-    migrations: {
-      directory: "./migrations",
-    },
-    seeds: {
-      directory: "./seeds",
-    },
+  },
+  // connection: config.POSTGRESS_URL,
+  migrations: {
+    directory: "./migrations",
+  },
+  seeds: {
+    directory: "./seeds",
   },
 };
