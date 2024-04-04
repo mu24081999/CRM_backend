48f9369ce465b6ffc9e7e3f34776c57c0e78711e
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-04-04 06:49:53 +0500

    adfadkf

--- server.js
+++ server.js
@@ -100,21 +100,23 @@ app.use(
 //     methods: ["GET", "POST", "PUT", "DELETE"],
 //   })
 // );
-// Define an array of allowed origins
 
-// Configure CORS middleware
-app.use((req, res, next) => {
-  const allowedOrigins = ["http://desktopcrm.com", "http://203.161.50.83"];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
-  return next();
-});
+var allowedDomains = ["http://desktopcrm.com", "http://203.161.50.83"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // bypass the requests with no origin (like curl requests, mobile apps, etc )
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 //error handling middleware
 app.use((error, req, res, next) => {
   logger.error(`Error :: ${req.originalUrl} :: ${error}`);
