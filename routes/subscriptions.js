c019731361d958b34119a2c9fdb89f8ee444de0b
mu24081999 <116753849+mu24081999@users.noreply.github.com>
2024-07-26 17:07:17 +0500

    updated

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -16,7 +16,7 @@ router.post(
   addSubscription
 );
 router.put(
-  "/updated-subscription/:subscription_id",
+  "/update-subscription/:subscription_id",
   IsAuth,
   authorizedRole(["SUPER_ADMIN", "USER", "ADMIN", "AGENT"]),
   updateSubscription
